75eccfb58d481f84fc298a45759faef59d62172b
EdDvd <davidgripho@gmail.com>
2021-03-03 10:38:16 -0600

    slightChanges

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODBURI,{useNewUrlParser:true,useUnifiedTopology:true, useFindAndModify:false})
 .then(
- db=> console.log('conexion exitosa--'+process.env.MONGODBURI)
+ db=> console.log('conexion exitosa--')
 )
 
 .catch(
